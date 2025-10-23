74f20b8b5006505c39dc83a41841d16fc577f70e
Soatok Dreamseeker <soatok.dhole@gmail.com>
2025-10-22 20:20:00 -0400

    Tighten type profile for Merkle Tree implementation
    Use data type classes instead of just arrays

---
+++ src/Merkle/ConsistencyProof.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+namespace FediE2EE\PKD\Crypto\Merkle;
+
+use Override;
+
+class ConsistencyProof implements \JsonSerializable
+{
+    /**
+     * @param string[] $proof
+     */
+    public function __construct(
+        public readonly array $proof
+    ) {}
+
+    #[Override]
+    public function jsonSerialize(): array
+    {
+        return $this->proof;
+    }
+}
