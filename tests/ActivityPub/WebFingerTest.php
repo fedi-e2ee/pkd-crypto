2b3f14ef70fed7a3e7df7c8ef21e6b81b7a0513c
Soatok Dreamseeker <soatok.dhole@gmail.com>
2025-11-08 19:11:39 -0500

    feat: canonicalize Actor IDs

---
+++ tests/ActivityPub/WebFingerTest.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+namespace FediE2EE\PKD\Crypto\Tests\ActivityPub;
+
+use FediE2EE\PKD\Crypto\ActivityPub\WebFinger;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(WebFinger::class)]
+class WebFingerTest extends TestCase
+{
+    public static function knownAnswers(): array
+    {
+        return [
+            ['@fedie2ee@mastodon.social', 'https://mastodon.social/ap/users/115428847654719749'],
+            ['@soatok@furry.engineer', 'https://furry.engineer/users/soatok'],
+            ['@evan@cosocial.ca', 'https://cosocial.ca/users/evan'],
+        ];
+    }
+
+    #[DataProvider("knownAnswers")]
+    public function testKnownAnswers(string $input, string $expected): void
+    {
+        $actual = (new WebFinger())->canonicalize($input);
+        $this->assertSame($expected, $actual, $input);
+    }
+}
