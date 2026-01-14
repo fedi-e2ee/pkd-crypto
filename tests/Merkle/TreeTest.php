72f628f80cf8b3344e5d7ab4c4e4ba917dab17cf
Soatok Dreamseeker <soatok.dhole@gmail.com>
2026-01-14 11:30:02 -0500

    Improve test coverage for mutation tests

--- tests/Merkle/TreeTest.php
+++ tests/Merkle/TreeTest.php
@@ -248,4 +248,86 @@ class TreeTest extends TestCase
         $this->expectExceptionMessage('This hash function is not permitted: ' . $hashAlg);
         new Tree([], $hashAlg);
     }
+
+    /**
+     * @throws CryptoException
+     * @throws SodiumException
+     */
+    #[DataProvider("hashAlgProvider")]
+    public function testKnownAnswerInternal(string $hashAlg): void
+    {
+        // Start with some leaves
+        $tree = new Tree([
+            sodium_bin2hex('76b53f0355924d554637fa12f0013594627e961c85898ea4d405921a0692fe66a17c4cf3085994ae8c337770b8f62acb50065e5814f784f37e2234af6d03995f'),
+            sodium_bin2hex('f0744c1e6ff6c54283e485b18b595ad183b93f01906f29df36127ff69ef4c9b7'),
+            sodium_bin2hex('9ec1764dfdb6d8ff511c94cdf3496d7259f97514701428d52d8d0cf05d260c73'),
+            sodium_bin2hex('6a313a049b6fedb0b9e156527ed80cc42d342b2c7bda8c1db87d21c2e561468b'),
+        ], $hashAlg);
+        $root = $tree->getEncodedRoot();
+        $expected1 = match ($hashAlg) {
+            'blake2b' => 'pkd-mr-v1:eS9FxLEc9YTNiesIOL1cono1n7u3VuH4WRm9hPGLA9I',
+            'sha256' => 'pkd-mr-v1:CH0-7A8ry0FMVD7i7urGHqcTuL4ueMlYmXxE0bzxwCI',
+            'sha384' => 'pkd-mr-v1:48tTulgIWNDjKT-aSVmCL8hW1BbXDmLR7UXyuwTWYy4msh-rEf1WlQgeiHmXCv_L',
+            'sha512' => 'pkd-mr-v1:czy8MCjUDCzsu0-d7OHRdXf2y58tszxFXfaEZCP4bv1sJK7yCgL1LUKvXdPbwOqatjQqBaYpt8k2WCErsOz1tw',
+        };
+        $this->assertSame($expected1, $root);
+
+        // Add another leaf
+        $newLeaf = sodium_bin2hex('9c1900e20895cc9b92d82d94aa9117432fe930a56d02015dbca7ab0fc49d1834');
+        $tree->addLeaf($newLeaf);
+
+        $root = $tree->getEncodedRoot();
+        $expected2 = match ($hashAlg) {
+            'blake2b' => 'pkd-mr-v1:fUkSRnyXBBWO7dN5c0DF6ez4KNexSArLlotB9x3M_Rk',
+            'sha256' => 'pkd-mr-v1:pMuu_3rgLYrVc5Nljatotbo9Mue7qp437Cg_0NAq3h0',
+            'sha384' => 'pkd-mr-v1:4IkmMGSWp2mvNLhehZ8IKu9uisaBd5vTdFHPoVzlzzmmbi6MuhvGPr9xm8O2pk-Y',
+            'sha512' => 'pkd-mr-v1:IPYsZtgnh2DwBFgSm8vtVVyBf3bv_FrfXpM6xdJU3uYJioo4ZCH8rFyv_EafWJNSX4hZL8IVnPL_VPZ-WttEMg',
+        };
+        $this->assertSame($expected2, $root);
+
+        // Generate and verify an inclusion proof:
+        $inclusion = $tree->getInclusionProof($newLeaf);
+        $this->assertTrue($tree->verifyInclusionProof($tree->getRoot(), $newLeaf, $inclusion));
+    }
+
+    public static function splitPointProvider(): array
+    {
+        return [
+            [0, 0],
+            [1, 0],
+            [2, 1],
+            [3, 2],
+            [4, 2],
+            [5, 4],
+            [6, 4],
+            [7, 4],
+            [8, 4],
+            [9, 8],
+            [16, 8],
+            [17, 16],
+            [32, 16],
+            [33, 32],
+            [64, 32],
+            [65, 64],
+            [128, 64],
+            [129, 128],
+            [192, 128],
+            [256, 128],
+        ];
+    }
+
+    #[DataProvider("splitPointProvider")]
+    public function testGetSplitPoint(int $input, $expected): void
+    {
+        $this->assertSame($expected, Tree::getSplitPoint($input));
+    }
+
+    public function testIsHashFunctionAllowed(): void
+    {
+        $this->assertFalse(Tree::isHashFunctionAllowed('crc32'));
+        $this->assertFalse(Tree::isHashFunctionAllowed('adler32'));
+        $this->assertFalse(Tree::isHashFunctionAllowed('md5'));
+        $this->assertFalse(Tree::isHashFunctionAllowed('sha1'));
+        $this->assertTrue(Tree::isHashFunctionAllowed('blake2b'));
+    }
 }
