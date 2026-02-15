b3ca1443032a2bd90e20f241ef3a3aeba8569a8d
Soatok Dreamseeker <soatok.dhole@gmail.com>
2026-02-15 02:26:17 -0500

    Setup php-cs-fixer with soatok/code-style

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+use Soatok\CodeStyle\SoatokRules;
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([
+        __DIR__ . '/src',
+        __DIR__ . '/tests',
+    ])
+;
+
+return (new PhpCsFixer\Config())->setRules(SoatokRules::asArray())->setFinder($finder);
